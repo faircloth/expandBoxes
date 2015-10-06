c63cd7014bd4ff653b8516f49f2b458d7f3cafb6
Andrew Faircloth <andrewkfaircloth@gmail.com>
2015-10-06 17:07:08 -0400

    sibling at parent level

--- main.js
+++ main.js
@@ -3,11 +3,20 @@
 
 
 $('div').on('click','.headers', function(event) {
-    btn = $(event.target);
-    parent = $(btn.parent());
-    content = $(parent.children('p'));
-    content.removeClass('content')
-    content.addClass('hidden');
+    var btn = $(event.target);
+    var parent = $(btn.parent());
+    var content = $(parent.children('p'));
+    content.removeClass('hidden')
+    content.addClass('shown');    
+});
+
+$('div').on('click','.headers', function(event) {
+    var btn = $(event.target);
+    var parent = $(btn.parent());
+    var sibling = $(parent.siblings());
+    var content = $(sibling.children('p'));
+    content.removeClass('shown')
+    content.addClass('hidden');    
 });
 
 
